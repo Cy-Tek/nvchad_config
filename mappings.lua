d877e455c072e16b5936797d3ee5a9f29c220e21
Josh Hannaford <hannaford.joshua55@gmail.com>
2023-05-04 09:27:57 -0400

    Add default command for formatting with LSP

--- mappings.lua
+++ mappings.lua
@@ -6,6 +6,12 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>fp"] = { ":Telescope projects<cr>", "Search projects", opts = { nowait = true } },
     ["<leader>q"] = { ":q<cr>", opts = { nowait = true } },
+    ["<leader>fm"] = {
+      function()
+        vim.lsp.buf.format { async = true }
+      end,
+      "Format with LSP",
+    }
   },
 }
 
